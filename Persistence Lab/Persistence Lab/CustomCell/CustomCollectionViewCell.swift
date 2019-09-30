ddf5eeda669d18658dd061302f4989782a06750e
Michelle <Michelle.cueva@gmail.com>
2019-09-30 15:50:01 -0400

    Creates cocoa files for view controllers

---
+++ Persistence Lab/Persistence Lab/CustomCell/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  Persistence Lab
+//
+//  Created by Michelle Cueva on 9/30/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
