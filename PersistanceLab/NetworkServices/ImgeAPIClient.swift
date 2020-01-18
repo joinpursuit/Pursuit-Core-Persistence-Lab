623dcaa56c206663ba17ec4ccdadb0f4c2783c86
LubaKaper <liubovkaper@pursuit.com>
2020-01-18 14:07:57 -0500

    networkHelper, Model

---
+++ PersistanceLab/NetworkServices/ImgeAPIClient.swift
@@ -0,0 +1,15 @@
+//
+//  ImgeAPIClient.swift
+//  PersistanceLab
+//
+//  Created by Liubov Kaper  on 1/18/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import Foundation
+
+struct ImageAPIClient {
+    static func getAllImageInfo(completion: @escaping (Result<[Hit], AppError>) ->()) {
+        
+    }
+}
---
+++ PersistanceLab/PersistanceLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersistanceLab
+//
+//  Created by Liubov Kaper  on 1/18/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
