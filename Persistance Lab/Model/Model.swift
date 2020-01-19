a295d3551a2bbf34d5aa3f9d6d93e4551407e56b
Tsering Lama <tseringlama@pursuit.org>
2020-01-19 15:08:51 -0500

    setting up folders

---
+++ Persistance Lab/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Persistance Lab
+//
+//  Created by Tsering Lama on 1/19/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import Foundation
---
+++ Persistance Lab/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Persistance Lab
+//
+//  Created by Tsering Lama on 1/19/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
