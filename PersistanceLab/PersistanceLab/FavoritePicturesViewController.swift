8fae39f284442303fdbc74424b5bc02090135f4b
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-23 16:16:11 -0500

    setting VCs

--- PersistanceLab/PersistanceLab/FavoritePicturesViewController.swift
+++ PersistanceLab/PersistanceLab/FavoritePicturesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritePicturesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
