67eeb45db4d8a1ffd2d3e1a4ae6a872a1eef2b2c
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-24 17:40:27 -0500

    set UI for the lab

---
+++ PersistanceLab/PersistanceLab/PicsSearchCell.swift
@@ -0,0 +1,16 @@
+//
+//  PicsSearchCellCollectionViewCell.swift
+//  PersistanceLab
+//
+//  Created by Yuliia Engman on 1/24/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class PicsSearchCell: UICollectionViewCell {
+    
+    @IBOutlet weak var searchedImage: UIImageView!
+    
+    
+}
