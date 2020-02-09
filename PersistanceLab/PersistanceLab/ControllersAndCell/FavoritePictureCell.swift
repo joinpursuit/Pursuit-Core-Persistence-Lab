590a197f364152f57861594908162520d2e683eb
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-09 14:45:54 -0500

    set favorite Picture VC

---
+++ PersistanceLab/PersistanceLab/ControllersAndCell/FavoritePictureCell.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritePictureCell.swift
+//  PersistanceLab
+//
+//  Created by Yuliia Engman on 2/9/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePictureCell: UITableViewCell {
+
+    @IBOutlet weak var favoritePictureImage: UIImageView!
+    
+}
