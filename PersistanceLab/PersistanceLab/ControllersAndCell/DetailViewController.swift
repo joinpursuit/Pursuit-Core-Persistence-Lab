d5db21c832db7894e40455a354f159cf4c89bb0b
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-09 14:34:18 -0500

    pictures loads to detail VC

--- PersistanceLab/PersistanceLab/ControllersAndCell/DetailViewController.swift
+++ PersistanceLab/PersistanceLab/ControllersAndCell/DetailViewController.swift
@@ -18,11 +18,39 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var webformatURLLabel: UILabel!
     @IBOutlet weak var previewURLLabel: UILabel!
     
+    @IBOutlet weak var favoriteBarButton: UIBarButtonItem!
+    
+    
     var onePicture: Hit?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
-
-
+    
+    private func updateUI() {
+        guard let onePicture = onePicture else {
+            fatalError("did not load data from segue")
+        }
+        tagsLabel.text = "Tags: \(onePicture.tags)"
+        likesLabel.text = "Likes: \(String(onePicture.likes))"
+        favoriteLabel.text = "Favorite: \(String(onePicture.favorites))"
+        webformatURLLabel.text = "WebformatURL: \(onePicture.webformatURL)"
+        previewURLLabel.text = "PreviewURL: \(onePicture.previewURL)"
+        
+        detailImage.getImage(with: onePicture.largeImageURL) {
+            [weak self] (result) in
+            switch result {
+            case .failure:
+                print("could not load picture")
+            case .success(let picture):
+                DispatchQueue.main.async {
+                    self?.detailImage.image = picture
+                }
+            }
+        }
+    }
 }
+
+
+
